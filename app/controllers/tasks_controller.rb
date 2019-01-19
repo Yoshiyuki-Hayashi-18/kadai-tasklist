e22764e6f17cb12f4b44b10677bd7c2011950a3b
yuzupon <padle.kyy.rr@gmail.com>
2019-01-19 12:17:07 +0000

    tasks_controller.rb edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,6 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
 private
 
@@ -54,4 +53,6 @@ end
 
 def task_params
   params.require(:task).permit(:content, :status)
+end
+
 end
