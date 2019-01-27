4e670999f63f18583063c051f0bbfd1c5cbbded4
yuzupon <padle.kyy.rr@gmail.com>
2019-01-27 13:47:55 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
-  root to: 'toppages#index'
+  root to: 'tasks#index'
+  
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+  
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+  
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :new, :create]
+    
 end
